211e067b548d0120c3284b0617cad1908c9a6053
Tarek Awwad <tarek.awwad.36@gmail.com>
2019-12-31 16:51:10 +0100

    Configure tki transfer and create form for delegation

--- src/components/cosmos.vue
+++ src/components/cosmos.vue
@@ -7,10 +7,11 @@
       <div class="main-container transfer-container">
         <div class="tabs">
           <!-- 转账tab -->
-          <a class="tab">{{$t("transfer")}}</a>
+          <a class="tab" href="#transfer-form">{{$t("transfer")}}</a>
+          <a class="tab" href="#delegate-form">{{$t("delegatetx")}}</a>
         </div>
         <!-- 转账内容 -->
-        <div class="transfer">
+        <div id="transfer-form" class="transfer">
           <form class="basic-form">
             <!-- 接收地址 -->
             <label>{{$t("webwallet_to_address")}}</label>
@@ -44,13 +45,13 @@
                     <div class="fast">{{$t("webwallet_fast")}}</div>
                 </li>
                 <li class="token">
-                  <div class="input">{{transfer.fee.toFixed(6)}} ATOM</div>
+                  <div class="input">{{transfer.fee.toFixed(6)}} TKI</div>
                 </li>
               </ul>
               <!-- 高级设置 -->
               <ul class="basic-group clearfix" v-show="selectedSet==2">
                 <li class='gas-price'>
-                  <span>Gas Price (UATOM)</span>
+                  <span>Gas Price (TKI)</span>
                   <input type="text" placeholder="0" v-model="transfer.gasPrice">
                 </li>
                 <li class="gas-limit">
@@ -58,7 +59,7 @@
                   <input type="text" placeholder="0" v-model="transfer.gasLimit">
                 </li>
                 <li class="token">
-                  <div class="input">{{feeCompute.toFixed(6)}} ATOM</div>
+                  <div class="input">{{feeCompute.toFixed(6)}} TKI</div>
                 </li>
               </ul>
             </div>
@@ -68,6 +69,71 @@
             <a class="btn" @click="sendTransfer">{{$t("transfer")}}</a>
           </form>
         </div>
+<hr>
+
+<!-- ==================================================== -->
+<div id="delegate-form" class="transfer">
+  <form class="basic-form">
+    <!-- 接收地址 -->
+    <label>{{$t("webwallet_to_address")}}</label>
+    <input type="text" :placeholder="$t('webwallet_to_address_pl')" v-model="transfer.account">
+    <ul class="basic-group clearfix">
+      <li class='amount'>
+        <!-- 转账金额 -->
+        <label>{{$t("transfer_amount")}}</label>
+        <input type="text" placeholder="0" v-model="transfer.amount">
+      </li>
+      <li class="token">
+        <!-- Token -->
+        <label>Token</label>
+        <select v-model="transfer.token">
+          <option v-for="item in values" :value="item" :key="item">{{item}}</option>
+        </select>
+      </li>
+    </ul>
+    <div class="fee-set">
+      <!-- 手续费 -->
+      <label>{{$t("webwallet_fee")}}</label>
+      <label v-show="selectedSet==2" class="setBtn" @click="setToggle(1)">{{$t("webwallet_simple")}}</label>
+      <label v-show="selectedSet==1" class="setBtn" @click="setToggle(2)">{{$t("webwallet_advanced")}}</label>
+      <!-- 普通设置 -->
+      <ul v-show="selectedSet==1" class="basic-group clearfix">
+        <li class="amount slider" ref="slider">
+          <div class="thunk" :style="{left}" ref="thunk">
+            <div class="block"><img src="static/img/icons/slider@2x.png" width="16"></div>
+            </div>
+            <div class="cheap">{{$t("webwallet_cheap")}}</div>
+            <div class="fast">{{$t("webwallet_fast")}}</div>
+        </li>
+        <li class="token">
+          <div class="input">{{transfer.fee.toFixed(6)}} TKI</div>
+        </li>
+      </ul>
+      <!-- 高级设置 -->
+      <ul class="basic-group clearfix" v-show="selectedSet==2">
+        <li class='gas-price'>
+          <span>Gas Price (TKI)</span>
+          <input type="text" placeholder="0" v-model="transfer.gasPrice">
+        </li>
+        <li class="gas-limit">
+          <span>Gas Limit</span>
+          <input type="text" placeholder="0" v-model="transfer.gasLimit">
+        </li>
+        <li class="token">
+          <div class="input">{{feeCompute.toFixed(6)}} TKI</div>
+        </li>
+      </ul>
+    </div>
+    <!-- 备注 -->
+    <label>{{$t("memo")}}</label>
+    <input type="text" :placeholder="$t('webwallet_memo_pl')" v-model="transfer.memo">
+    <a class="btn" @click="sendDelegateTx">{{$t("delegatetx")}}</a>
+  </form>
+</div>
+
+<!-- ==================================================== -->
+
+
       </div>
     </section>
   </template>
@@ -81,9 +147,9 @@ import common from 'static/js/common.js'
 export default {
   data() {
     return {
-      blockchain:'Cosmos',
+      blockchain:'kichain',
       account: '',
-      network: this.globalData.cosmos.network,
+      network: this.globalData.kichain.network,
       unit: this.webCoin.unit,
       selectedSet: 1,
       slider: null, //滚动条DOM元素
@@ -96,11 +162,11 @@ export default {
       transfer: {
         'account': '',
         'amount': 0,
-        'token': 'ATOM',
+        'token': 'tki',
         'memo': '',
         'fee': 0.001250,
         'gasPrice': '0.0000005',
-        'gasLimit': 30000
+        'gasLimit': 100000
       },
       account_number: 0,
       sequence: 0,
@@ -168,8 +234,10 @@ export default {
   },
   methods: {
     getAccount() {
-      if (this.webUtil.getCookie('identity_cosmos')) {
-        this.account = JSON.parse(this.webUtil.getCookie('identity_cosmos')).account;
+    // console.log(this.webUtil.getCookie('identity_kichain'))
+      if (this.webUtil.getCookie('identity_kichain')) {
+        this.account = JSON.parse(this.webUtil.getCookie('identity_kichain')).account;
+        // this.account = 'cosmos10mdlxmrewp2vjlyly2q8yp44aqau0re5xuz3l9'
         this.initExtension()
       }
     },
@@ -214,11 +282,12 @@ export default {
       }
     },
     initExtension() {
-      let nodeUrl = this.globalData.cosmos.nodeUrl;
+      let nodeUrl = this.globalData.kichain.nodeUrl;
       this.webUtil.initMathExtension().then((res) => {
         let account = this.account;
         this.progressSlide();
         let provider = mathExtension.httpProvider(nodeUrl);
+        // console.log(provider)
         let promise1 = new Promise((resolve, reject) => {
           //获取ATOM价格
           this.$http.get('https://api.coinmarketcap.com/v1/ticker/cosmos/?convert=CNY').then(res => {
@@ -235,36 +304,38 @@ export default {
         var promise2 = new Promise((resolve, reject) => {
           let krwPrice = 0;
           //获取汇率
-          this.$http.get(this.globalData.domain + 'api/baseCoinPub?v=1.0').then(res => {
-            if (res.data && res.data.success) {
-              let result = res.data.data.legal;
-              result.forEach((coin) => {
-                if (coin.alias == 'CNY') {
-                  this.coin.cny = coin.rmb;
-                } else if (coin.alias == 'USD') {
-                  this.coin.usd = coin.rmb;
-                } else if (coin.alias == 'KRW') {
-                  this.coin.krw = coin.rmb;
-                  krwPrice = coin.rmb;
-                }
-              });
-              resolve(krwPrice);
-            }
-          }, err => {
-            reject(err);
-          });
+          //this.$http.get(this.globalData.domain + 'api/baseCoinPub?v=1.0').then(res => {
+          //   if (res.data && res.data.success) {
+          //     let result = res.data.data.legal;
+          //     result.forEach((coin) => {
+          //       if (coin.alias == 'CNY') {
+          //         this.coin.cny = coin.rmb;
+          //       } else if (coin.alias == 'USD') {
+          //         this.coin.usd = coin.rmb;
+          //       } else if (coin.alias == 'KRW') {
+          //         this.coin.krw = coin.rmb;
+          //         krwPrice = coin.rmb;
+          //       }
+          //     });
+          //     resolve(krwPrice);
+          //   }
+          // }, err => {
+          //   reject(err);
+          // });
         });
         var promise3 = new Promise((resolve, reject) => {
           // 获取可用ATOM
+
           provider.get('/auth/accounts/' + account).then((res1) => {
             if (res1.result.result.value) {
               let res = res1.result.result.value;
+              console.log(res)
               this.account_number = res.account_number;
               this.sequence = res.sequence;
               let coins = res.coins;
               if(coins){
                 coins.forEach((coin) => {
-                  if (coin.denom == 'uatom') {
+                  if (coin.denom == 'tki') {
                     this.balances.list.available = parseFloat(coin.amount) / Math.pow(10,6);
                   }
                 });
@@ -329,6 +400,8 @@ export default {
         });
       })
     },
+
+
     sendTransfer() {
       if(!this.transfer.account){
         alert(this.$t('transfer_account_null'));
@@ -342,7 +415,7 @@ export default {
         alert(this.$t('transfer_amount_min')+Math.pow(10,-6));
         return false;
       }
-      let nodeUrl = this.globalData.cosmos.nodeUrl;
+      let nodeUrl = this.globalData.kichain.nodeUrl;
       this.webUtil.initMathExtension().then((res) => {
         return mathExtension.getIdentity(this.network);
       }).then((identity) => {
@@ -351,7 +424,7 @@ export default {
         // 获取手续费
         // 普通设置
         let fee = this.transfer.fee * Math.pow(10,6);
-        let limit = 30000;
+        let limit = 200000;
         // 高级设置
         if (this.selectedSet == 2) {
           fee = this.transfer.gasPrice * this.transfer.gasLimit * Math.pow(10,6);
@@ -359,11 +432,11 @@ export default {
         }
         let transaction = {
           from: account,
-          chain_id: "cosmoshub-3",
+          chain_id: "KiChain",
           account_number: this.account_number,
           sequence: this.sequence,
           fees: {
-            denom: "uatom",
+            denom: "tki",
             amount: fee
           },
           gas: limit,
@@ -372,7 +445,7 @@ export default {
           msg: {
             to: this.transfer.account,
             coins: [{
-              denom: "uatom",
+              denom: "tki",
               amount: this.transfer.amount*Math.pow(10,6)
             }]
           }
@@ -386,6 +459,84 @@ export default {
             }
           };
           provider.post('/txs?sync=true', null, opts).then(res => {
+            console.log('asdasd')
+            let result=res.result;
+            if (result.code) {
+              let log = JSON.parse(result.raw_log);
+              alert(log.message);
+            } else if (result.txhash) {
+              alert(this.$t('transfer_success'));
+              window.location.reload();
+            }
+          })
+        }).catch(e => {
+          console.log(e.message)
+          alert(this.$t('transfer_fail'));
+        })
+      })
+    },
+
+    // ====================================================
+
+    sendDelegateTx() {
+      if(!this.transfer.account){
+        alert(this.$t('transfer_account_null'));
+        return false;
+      }
+      if(!this.transfer.amount){
+        alert(this.$t('transfer_amount_null'));
+        return false;
+      }
+      if(this.transfer.amount<Math.pow(10,-6)){
+        alert(this.$t('transfer_amount_min')+Math.pow(10,-6));
+        return false;
+      }
+      let nodeUrl = this.globalData.kichain.nodeUrl;
+      this.webUtil.initMathExtension().then((res) => {
+        return mathExtension.getIdentity(this.network);
+      }).then((identity) => {
+        let account = identity.account;
+        let provider = mathExtension.httpProvider(nodeUrl);
+        // 获取手续费
+        // 普通设置
+        let fee = this.transfer.fee * Math.pow(10,6);
+        let limit = 200000;
+        // 高级设置
+        if (this.selectedSet == 2) {
+          fee = this.transfer.gasPrice * this.transfer.gasLimit * Math.pow(10,6);
+          limit = this.transfer.gasLimit;
+        }
+
+        var transaction = {
+          from: account,
+          chain_id: "KiChain",
+          account_number: this.account_number,
+          sequence: this.sequence,
+          fees: {
+              denom: "tki",
+              amount: 500
+          },
+          gas: limit,
+          memo: this.transfer.memo,
+          type: "delegate",
+          msg: {
+              validator_addr: this.transfer.account,
+              amount: {
+                  denom: "tki",
+                  amount: this.transfer.amount*Math.pow(10,6)
+              }
+          }
+        };
+
+        mathExtension.requestSignature(transaction, this.network).then(signedTransaction => {
+          const opts = {
+            data: signedTransaction,
+            headers: {
+              "Content-Type": "text/plain",
+            }
+          };
+          provider.post('/txs?sync=true', null, opts).then(res => {
+            console.log('asdasd')
             let result=res.result;
             if (result.code) {
               let log = JSON.parse(result.raw_log);
@@ -401,6 +552,8 @@ export default {
         })
       })
     }
+
+    // ====================================================
   },
   components: {
     sideBar,
