55a059ca67580de640bad4ed43102d6d251a8557
Tarek Awwad <tarek.awwad.36@gmail.com>
2020-01-02 14:32:52 +0100

    Cleaning !

--- src/base/nav.vue
+++ src/base/nav.vue
@@ -1,27 +1,5 @@
 <template>
 <nav class="math-nav clearfix">
-	<div class="float-right">
-    <a href="https://mathwallet.org" class="mobile" target="_blank">
-      <img src="static/img/icons/phone_blue@2x.png" height="16">
-      <span>{{$t('webwallet_mobile_app')}}</span>
-    </a>
-    <div class="dropdown">
-      <a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown" role="button">{{$t("lang")}}<span class="tri"></span></a>
-      <ul class="dropdown-menu">
-        <li @click="changeLang('en')">English</li>
-        <li @click="changeLang('cn')">中文</li>
-        <li @click="changeLang('ko')">한국어</li>
-      </ul>
-    </div>
-    <div class="dropdown">
-      <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="cUnit">{{unit}}</span> <span class="caret"></span></a>
-      <ul class="dropdown-menu">
-        <li @click="toggleUnit('USD')">USD</li>
-        <li @click="toggleUnit('CNY')">CNY</li>
-        <li @click="toggleUnit('KRW')">KRW</li>
-      </ul>
-    </div>
-  </div>
   <a class="logo" href="https://mathwallet.org" target="_blank"><img src="static/img/icons/mathwallet_webwallet@2x.png" height="32"></a>
 </nav>
 </template>
