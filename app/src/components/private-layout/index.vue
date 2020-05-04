1d8ea1237e7c25d5e065d90705ecceb7be5dfe41
podotki <arnaud@foundation.ki>
2020-05-04 18:33:25 +0200

    Fixed height

--- app/src/components/private-layout/index.vue
+++ app/src/components/private-layout/index.vue
@@ -1,18 +1,18 @@
 <template>
-  <b-container fluid class="p-5">
+  <b-container class="py-5 h-100">
     <b-row>
-      <b-col class="pr-0 mr-lg-3" cols="1">
+      <b-col class="pr-0" :style="{ marginRight: '20px' }" cols="1">
         <CornerLogo />
       </b-col>
       <b-col class="pl-0">
         <TopBar />
       </b-col>
     </b-row>
-    <b-row class="mt-3">
-      <b-col class="pr-0 mr-lg-3" cols="1">
+    <b-row :style="{ marginTop: '15px', height: 'calc(100% - 80px)' }">
+      <b-col class="pr-0" cols="1" :style="{ marginRight: '20px' }">
         <SideMenu />
       </b-col>
-      <b-col class="pl-0">
+      <b-col class="pl-0 h-100">
         <Content>
           <router-view />
         </Content>
