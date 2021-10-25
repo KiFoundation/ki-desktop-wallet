bc987599f9ccceaa09447163d402c22cff176576
Tarek Awwad <tarek.awwad.36@gmail.com>
2021-10-25 11:25:33 +0200

    Added import with privkey to login

--- app/src/views/login/index.vue
+++ app/src/views/login/index.vue
@@ -198,10 +198,23 @@ export default {
     },
 
     handleImportWallet(formValue) {
-      const { wallet_name, wallet_pass_tmp, mnemonic, multisig, offline } = formValue;
+      const { wallet_name, wallet_pass_tmp, mnemonic, private_key, multisig, offline, address} = formValue;
 
-      // Create the wallet
-      const wallet = createWalletFromMnemonic(mnemonic, '', this.prefix);
+      var wallet = {}
+      var priv_key = ''
+
+      if ( mnemonic != null &&  private_key == null){
+        // Create the wallet
+        const wallet_temp = createWalletFromMnemonic(mnemonic, '', this.prefix);
+        wallet = wallet_temp
+        priv_key = wallet.privateKey.toString('hex')
+      }
+
+      if ( mnemonic == null &&  private_key != null){
+        priv_key = private_key
+        wallet.publicKey = secp256k1PublicKeyCreate(Buffer.from(private_key, 'hex'), true);
+        wallet.address = createAddress(wallet.publicKey, this.prefix);
+      }
 
       // Store Wallet
       this.storeInWalletList(wallet_name);
@@ -209,7 +222,7 @@ export default {
       // Encrypt the private key
       if (!offline){
         var encrypted_key = AES.encrypt(
-          wallet.privateKey.toString('hex'),
+          priv_key,
           wallet_pass_tmp,
         ).toString();
 
